6a377575b73f316fd9230034b7f845bb98b15dab
atulgittest <42926892+atulgittest@users.noreply.github.com>
2022-12-23 18:32:43 +0530

    software install

--- php.sh
+++ php.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 sudo apt update
+echo "update compeleted"
+sudo sudo apt-get install software-properties-common -y;sudo add-sudo apt-repository ppa:ondrej/php -y;sudo sudo apt-get update -y;sudo sudo apt-get install -y php7.4;sudo sudo apt-get install php7.4-mysql php7.4-curl php7.4-json php7.4-cgi php7.4-xsl php7.4-zip php7.4-mbstring php7.4-fpm php7.4-bcmath php7.4-cli -y;sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools -y;sudo apt install python3-venv -y;pip install wheel -y
+;sudo apt install nginx -y;sudo apt install php7.4-fpm -y;sudo apt install unzip -y;sudo apt install zip -y;sudo apt install net-tools -y;
+echo "php7.4 & nginx install ,"
+php -v
+
